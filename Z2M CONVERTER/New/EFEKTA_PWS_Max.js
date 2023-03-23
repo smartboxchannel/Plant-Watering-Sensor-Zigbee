547acc3e125378940ca08c67e55bf598e091e1fa
Andrew Lamchenko <31867646+smartboxchannel@users.noreply.github.com>
2023-03-23 21:13:15 +0300

    Add files via upload

--- Z2M CONVERTER/New/EFEKTA_PWS_Max.js
+++ Z2M CONVERTER/New/EFEKTA_PWS_Max.js
@@ -21,6 +21,19 @@ const tzLocal = {
             };
         },
     },
+	temperaturef_config: {
+        key: ['temperature_offset'],
+        convertSet: async (entity, key, rawValue, meta) => {
+            const value = parseFloat(rawValue)*10;
+            const payloads = {
+                temperature_offset: ['msTemperatureMeasurement', {0x0410: {value, type: 0x29}}],
+            };
+            await entity.write(payloads[key][0], payloads[key][1]);
+            return {
+                state: {[key]: rawValue},
+            };
+        },
+    },
 };
 
 const fzLocal = {
@@ -35,6 +48,17 @@ const fzLocal = {
             return result;
         },
     },
+	temperaturef_config: {
+        cluster: 'msTemperatureMeasurement',
+        type: ['attributeReport', 'readResponse'],
+        convert: (model, msg, publish, options, meta) => {
+            const result = {};
+            if (msg.data.hasOwnProperty(0x0410)) {
+                result.temperature_offset = parseFloat(msg.data[0x0410])/10.0;
+            }
+            return result;
+        },
+    },
 };
 
 const definition = {
@@ -42,8 +66,8 @@ const definition = {
         model: 'EFEKTA_PWS_Max',
         vendor: 'Custom devices (DiY)',
         description: '[Plant watering sensor EFEKTA PWS max](http://efektalab.com/PWS_Max)',
-        fromZigbee: [fz.temperature, fz.humidity, fz.illuminance, fz.soil_moisture, fz.battery, fzLocal.node_config],
-        toZigbee: [tz.factory_reset, tzLocal.node_config],
+        fromZigbee: [fz.temperature, fz.humidity, fz.illuminance, fz.soil_moisture, fz.battery, fzLocal.node_config, fzLocal.temperaturef_config],
+        toZigbee: [tz.factory_reset, tzLocal.node_config, tzLocal.temperaturef_config],
         configure: async (device, coordinatorEndpoint, logger) => {
             const firstEndpoint = device.getEndpoint(1);
             await reporting.bind(firstEndpoint, coordinatorEndpoint, [
@@ -51,7 +75,9 @@ const definition = {
         },
         exposes: [e.soil_moisture(), e.battery(), e.battery_low(), e.battery_voltage(), e.temperature(), e.humidity(), e.illuminance(), e.illuminance_lux(),
 		    exposes.numeric('report_delay', ea.STATE_SET).withUnit('Minutes').withDescription('Adjust Report Delay. Setting the time in minutes, by default 30 minutes')
-                .withValueMin(1).withValueMax(360),],
+                .withValueMin(1).withValueMax(360),
+		    exposes.numeric('temperature_offset', ea.STATE_SET).withUnit('°C').withValueStep(0.1).withDescription('Adjust temperature')
+                .withValueMin(-50.0).withValueMax(50.0)],
 };
 
 module.exports = definition;
